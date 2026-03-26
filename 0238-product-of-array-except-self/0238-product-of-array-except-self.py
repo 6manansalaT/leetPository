92e78bb552e8db5e1dc44e9f9fe4cd4965f1c342
Trinity Joy Manansala <54867806+6manansalaT@users.noreply.github.com>
2026-03-26 16:56:51 +0800

    Time: 550 ms (5.07%), Space: 31.4 MB (5.75%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # iterate through array once
+        # prefix and post fix
+
+        res_arr = [1] * len(nums)
+        pref = 1
+        post = 1
+
+        for i in range(len(nums)):
+            res_arr[i] = pref
+            pref *= nums[i]
+        print(res_arr)
+        for j in range(len(nums) - 1, -1, -1):
+            res_arr[j] *= post
+            post *= nums[j]
+        return res_arr
